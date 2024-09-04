a5d0fb7d768e3cab9f9c53fd9bb8adfcce32afd3
unknown <Bhavani-10kc>
2024-09-04 22:40:33 +0530

    javascript

---
+++ task1.js
@@ -0,0 +1,23 @@
+// Task 1: Print Numbers from 1 to 10
+for(var i=1;i<=10;i++){
+    console.log(i);
+}
+// Task 2: Print Multiplication Table
+var num=5
+for(var i=1;i<=10;i++){
+    console.log(num + "x"+ i+ "=" +(num*i));
+}
+// Task 3: Sum of First 10 Natural Numbers
+var sum=0;
+for(var i=1; i <=10; i++){
+    sum+= i;
+}
+console.log("The sum of the first 10 natural number is:",sum);
+// Task 4: Print Numbers Divisible by 3 from 1 to 30
+for(var i=3;i<=30;i=i+3){
+    console.log(i);
+}
+// Task 5: Print Square of Numbers from 1 to 10
+for(var i=1;i<=10;i++){
+    console.log(i + "squard is"+i**2);
+}
